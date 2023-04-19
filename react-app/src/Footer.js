3b0ffbe9aab5d374109b2021473136faa7f65fc8
Mollyphobia <123973226+Mollyphobia@users.noreply.github.com>
2023-04-18 21:59:20 -0500

    Lab 1

---
+++ react-app/src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+class Footer extends React.Component {
+    render() {
+        return (
+            <footer>
+                <p>Author: Mollemira Porphura</p>
+                <p><a href="mollyporphura@gmail.com">mollyporphura@gmail.com</a></p>
+            </footer>
+        )
+    }
+}
+
+export default Footer;
---
+++ react-app/src/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+class Header extends React.Component {
+    render() {
+        return (
+            <h2 style= {{textAlign: 'center'}}>LIST OF HORNED BEASTS</h2>
+        )
+    }
+}
+
+export default Header;
